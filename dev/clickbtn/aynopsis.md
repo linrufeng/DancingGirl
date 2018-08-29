9c07d5182f8c8b3cd2d9af4515713628c18e54fa
李林森 <lilinsen@jd.com>
2018-08-29 17:40:34 +0800

    波纹单击

---
+++ dev/clickbtn/aynopsis.md
@@ -0,0 +1,6 @@
+<link href="./css/clickbtn.css">
+
+# 按钮波纹效果
+[demo]('../html/index.html' '例子')
+
+<script src="./js/clickbtn.js"></script>
---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+console.log('hello world');
+const webpack = require('webpack');
+//基础配置
+const config = require('./package.json');
+//路径工具
+const path = require('path');
+//组件入口
+const component = require('./dev/components');
+console.log(component);
