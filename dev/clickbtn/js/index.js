43e89874939187d51e5611831ba11dd03d587edd
李林森 <lilinsen@jd.com>
2018-08-30 17:52:08 +0800

    按钮波纹效果

---
+++ dev/clickbtn/js/index.js
@@ -0,0 +1,55 @@
+class Waves{
+    constructor(options){
+        //获取name
+        this.name = options.name || "林如风";
+    }
+    requestAnimFrame () {
+        return (
+          window.requestAnimationFrame       || 
+          window.mozRequestAnimationFrame    || 
+          window.oRequestAnimationFrame      || 
+          window.msRequestAnimationFrame     || 
+          function (callback) {
+            window.setTimeout(callback, 1000 / 60);
+          }
+        );
+    }
+    init() {
+        containers = Array.prototype.slice.call(containers);
+        for (var i = 0; i < containers.length; i += 1) {
+          canvas = document.createElement('canvas');
+          canvas.addEventListener('click', press, false);
+          containers[i].appendChild(canvas);
+          canvas.style.width ='100%';
+          canvas.style.height='100%';
+          canvas.width  = canvas.offsetWidth;
+          canvas.height = canvas.offsetHeight;
+        }
+    }
+    press (event) {
+        color = event.toElement.parentElement.dataset.color;
+        element = event.toElement;
+        context = element.getContext('2d');
+        radius = 0;
+        centerX = event.offsetX;
+        centerY = event.offsetY;
+        context.clearRect(0, 0, element.width, element.height);
+        this.draw();
+    }    
+    draw() {
+        context.beginPath();
+        context.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
+        context.fillStyle = color;
+        context.fill();
+        radius += 2;
+        // 通过判断半径小于元素宽度，不断绘制 radius += 2 的圆形
+        if (radius < element.width) {
+          requestAnimFrame(draw);
+        }
+      };
+}
+let a = new Waves({
+    name:'李林森'
+})
+a.play()
+console.log(a.output())
